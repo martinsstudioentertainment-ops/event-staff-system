0b1a66b417ecb527e13ccce669ef4e1b6b876e31
Event Staff <deploy@event-staff.local>
2026-06-02 04:43:56 +0100

    Update summer roster

---
+++ database/live-events-2026.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Summer 2026 live event roster (master list for go-live).
+ *
+ * Apply: php database/sync-live-events.php
+ *        php database/sync-live-events.php --dry-run
+ */
+return [
+    'main_security_company' => '1Plus Security',
+    'events' => [
+    ['date' => '2026-06-10', 'name' => 'Nick Cave', 'location' => 'Malahide', 'staff_needed' => 94, 'times' => 'TBC'],
+    ['date' => '2026-06-13', 'name' => 'Kingfishr', 'location' => 'Malahide', 'staff_needed' => 94, 'times' => 'TBC'],
+    ['date' => '2026-06-19', 'name' => 'Metallica', 'location' => 'Malahide', 'staff_needed' => 118, 'times' => 'TBC'],
+    ['date' => '2026-06-20', 'name' => 'Kodaleone', 'location' => 'Aviva Stadium', 'staff_needed' => 52, 'times' => '15:00-23:00'],
+    ['date' => '2026-06-21', 'name' => 'Metallica', 'location' => 'Malahide', 'staff_needed' => 118, 'times' => 'TBC'],
+    ['date' => '2026-06-23', 'name' => 'Teddy Swoms', 'location' => 'Aviva Stadium', 'staff_needed' => 52, 'times' => '15:00-23:00'],
+    ['date' => '2026-06-24', 'name' => 'Katy Perry', 'location' => 'Malahide', 'staff_needed' => 118, 'times' => 'TBC'],
+    ['date' => '2026-06-26', 'name' => 'Lewis Capaldi', 'location' => 'Malahide', 'staff_needed' => 118, 'times' => 'TBC'],
+    ['date' => '2026-06-27', 'name' => 'Michael Buble', 'location' => 'Thomond Park', 'staff_needed' => 50, 'times' => '15:00-23:00'],
+    ['date' => '2026-06-27', 'name' => 'Florence & The Machine', 'location' => 'Malahide', 'staff_needed' => 94, 'times' => 'TBC'],
+    ['date' => '2026-06-28', 'name' => 'Calvin Harris', 'location' => 'Thomond Park', 'staff_needed' => 50, 'times' => '15:00-23:00'],
+    ['date' => '2026-06-28', 'name' => 'Michael Buble', 'location' => 'Malahide', 'staff_needed' => 136, 'times' => 'TBC'],
+    ['date' => '2026-06-30', 'name' => 'Maroon 5', 'location' => 'Malahide', 'staff_needed' => 108, 'times' => 'TBC'],
+    ['date' => '2026-07-01', 'name' => 'Kings Of Leon', 'location' => 'Thomond Park', 'staff_needed' => 50, 'times' => '15:00-23:00'],
+    ['date' => '2026-07-04', 'name' => 'Take That', 'location' => 'Aviva Stadium', 'staff_needed' => 52, 'times' => '15:00-23:00'],
+    ['date' => '2026-07-07', 'name' => 'Shania Twain', 'location' => 'Thomond Park', 'staff_needed' => 50, 'times' => '15:00-23:00'],
+    ['date' => '2026-07-08', 'name' => 'Pitbull', 'location' => 'Thomond Park', 'staff_needed' => 50, 'times' => '15:00-23:00'],
+    ['date' => '2026-07-11', 'name' => 'Dermot Kennedy', 'location' => 'Aviva Stadium', 'staff_needed' => 52, 'times' => '15:00-23:00'],
+    ['date' => '2026-07-12', 'name' => 'Dermot Kennedy', 'location' => 'Aviva Stadium', 'staff_needed' => 52, 'times' => '15:00-23:00'],
+    ['date' => '2026-07-18', 'name' => 'Luke Combs', 'location' => 'Slane', 'staff_needed' => 150, 'times' => 'TBC', 'staff_note' => '100-150'],
+    ['date' => '2026-07-19', 'name' => 'Luke Combs', 'location' => 'Slane', 'staff_needed' => 150, 'times' => 'TBC', 'staff_note' => '100-150'],
+    ['date' => '2026-08-20', 'name' => 'The Prodigy', 'location' => 'RHK', 'staff_needed' => 67, 'times' => '14:00-23:30'],
+    ['date' => '2026-08-21', 'name' => 'Moby & Faithless', 'location' => 'RHK', 'staff_needed' => 67, 'times' => '14:00-23:30'],
+    ['date' => '2026-08-22', 'name' => 'The Weekend', 'location' => 'Croke Park', 'staff_needed' => 74, 'times' => '15:00-23:00'],
+    ['date' => '2026-08-23', 'name' => 'The Weekend', 'location' => 'Croke Park', 'staff_needed' => 74, 'times' => '15:00-23:00'],
+    ['date' => '2026-08-25', 'name' => 'Deftones', 'location' => 'RHK', 'staff_needed' => 67, 'times' => '14:00-23:30'],
+    ['date' => '2026-08-27', 'name' => 'Electric Picnic', 'location' => 'Stradbally', 'staff_needed' => 71, 'times' => 'TBC'],
+    ['date' => '2026-08-28', 'name' => 'Electric Picnic', 'location' => 'Stradbally', 'staff_needed' => 139, 'times' => 'TBC'],
+    ['date' => '2026-08-29', 'name' => 'Electric Picnic', 'location' => 'Stradbally', 'staff_needed' => 139, 'times' => 'TBC'],
+    ['date' => '2026-08-30', 'name' => 'Electric Picnic', 'location' => 'Stradbally', 'staff_needed' => 139, 'times' => 'TBC'],
+    ['date' => '2026-08-30', 'name' => 'Bon Jovi', 'location' => 'Croke Park', 'staff_needed' => 74, 'times' => '15:00-23:00'],
+    ['date' => '2026-08-31', 'name' => 'Electric Picnic', 'location' => 'Stradbally', 'staff_needed' => 9, 'times' => 'TBC'],
+    ],
+];
